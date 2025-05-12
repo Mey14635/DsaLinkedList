4f609d659b961e73609f187667e3e0fa3f0d818a
Mey14635 <kasomediatrice1407@gmail.com>
2025-05-12 19:29:44 +0300

    commit 2

--- Linkedlist.py
+++ Linkedlist.py
@@ -2,8 +2,6 @@ class Node:
     def __init__(self, data=None, next=None):
         self.data = data
         self.next = next
-
-
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -11,3 +9,31 @@ class LinkedList:
     def insert_at_start(self, data):
         node = Node(data, self.head)
         self.head = node
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(data, None)
+            return
+
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+        itr.next = Node(data, None)
+
+    def display(self):
+        if self.head is None:
+            print("Linked list is empty")
+            return
+        itr = self.head
+        listr = ''
+        while itr:
+            listr += str(itr.data) + "-->"
+            itr = itr.next
+        print(listr)
+
+    def get_lenght(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count += 1
+            itr = itr.next
+        return count
